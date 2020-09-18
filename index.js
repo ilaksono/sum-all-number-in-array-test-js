2475928321180a0a799294c6c909f4bf163b2264
ilaksono <ian.laksono@gmail.com>
2020-09-18 16:54:20 +0000

    added sum array recursion function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = (array) => {
+  return array.reduce((a, element) => {
+    if (Array.isArray(element))
+      return sumItems(element) + a;
+    else
+      return element + a;
+  }, 0);
+  //
+  // sum = 0;
+
+  // for (let a of array) {
+  //   if(Array.isArray(a))
+  //     sum = sumItems(a);
+  //   else
+  //     sum += a;
+  // }
+  // return sum;
+};
+// console.log(sumItems([1,2,3,[4,[[5]]]]));
+console.log(sumItems([[1,2]]));
 
 module.exports = sumItems;
